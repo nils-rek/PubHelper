b6ba0b8ec9a5d44b3f9ba5a5762d87656cf1aa44
Nils Kappelmann <n.kappelmann@gmail.com>
2021-03-12 09:19:20 +0100

    started writing loopGLMTable

--- R/loopGLMTable.R
+++ R/loopGLMTable.R
@@ -4,8 +4,9 @@
 #' @param data Data.frame including model variables
 #' @param x Vector of predictor variables
 #' @param y Vector of outcome variables
-#' @param covars Vector of covariates
-#' @param ... Other arguments from PubHelper
+#' @param z Vector of covariates if covariates are included
+#' @param glm_fun Function for glm model
+#' @param ... Other arguments for GLM
 #' @keywords GLM; table
 #' @export
 #' @author Nils Kappelmann
@@ -18,10 +19,32 @@ loopGLMTable(
   data = NULL,
   x = NULL,
   y = NULL,
-  covars = NULL,
+  z = NULL,
+  glm_fun = lm,
   ...
 ) {
 
+  ## Check if input arguments are present
+  if(is.null(data))  {stop("data needs to be specified.")}
+  if(is.null(x))  {stop("x needs to be specified.")}
+  if(is.null(y))  {stop("y needs to be specified.")}
 
+  ## Get grid
+  output = expand.grid(y = y, x = x, stringsAsFactors = FALSE)
+
+  ## Get formula
+  output$formula = paste0(output$y, "~", output$x)
+  if(!is.null(z)) {
+    z.comb = paste(z, collapse = "+")
+    output$formula = paste(output$formula, z.comb, sep = "+")
+  }
+
+  ## Run models
+  output$models = map(output$formula, glm_fun, data = data)
+
+  ## Results
+  #output$GLMTables = map(models, PubHelper::getGLMTable)
+
+  return(output)
 
 }
